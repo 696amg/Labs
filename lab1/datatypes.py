389eb85086d7a99af0202943f4814999879bfe52
Artem <artem.nn.00@mail.ru>
2024-02-01 00:51:11 +0600

    first commit

---
+++ lab1/datatypes.py
@@ -0,0 +1,12 @@
+b = 9
+print(type(b)) 
+
+c = "KKKalk"
+print(type(c))
+
+x = 3.477
+print(type(x))
+
+g = ["large", "big"]
+print(type(g))
+
---
+++ lab1/variables.py
@@ -0,0 +1,22 @@
+carname = "Volvo"
+
+x = 50 
+
+x = 7
+y = 8
+print(x+y)
+
+x = 9
+y = 0
+z = x + y
+print(z)
+
+x, y, z = "Earth", "Mars", "Saturn"
+
+x = y = z = "Earth"
+
+def myfunc():
+    global kl
+    kl = "something"
+
+    
